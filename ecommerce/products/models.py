92ac4b6003525063642a0894cf8386ffa4988154
kelvin okumu <105194367+kelvinokumu@users.noreply.github.com>
2025-11-20 18:26:28 +0300

    product model and form

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -5,3 +5,14 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length = 200)
+    price  = models.DecimalField(max_digits= 10, decimal_places = 2, default = 0.00)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(blank = True)
+    image = models.ImageField(upload_to='/images')
+    stock = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
